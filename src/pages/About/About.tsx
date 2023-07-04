72bd6cf8671392e6526ba12caaa2911e373e92e8
dincerdegre <dincerdegre@gmail.com>
2023-07-04 18:58:09 +0300

    finished about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import MiddleLine from "../../components/AboutPage/Middleline/MiddleLine";
+import Blockquote from "../../components/Shared/Blockquote/Blockquote";
 
-const About : React.FC = () => {
+const About: React.FC = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <MiddleLine />
+      <Blockquote text="Welcome! Here imagination meets innovation, and your aspirations find their ultimate expression!" />
+    </>
+  );
+};
 
-export default About
+export default About;
