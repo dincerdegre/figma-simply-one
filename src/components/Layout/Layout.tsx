3084748fd711b2b1bdfafae48ce1da52bbd2c474
dincerdegre <dincerdegre@gmail.com>
2023-07-04 06:04:55 +0300

    changed layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import Header from './Header/Header';
-import Footer from './Footer/Footer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
-interface LayoutProps {
-    children: React.ReactNode;
-}
 
-const Layout: React.FC<LayoutProps> = ({children}) => {
+const Layout: React.FC = () => {
   return (
-    <div className='flex m-auto w-[1440px] pb-0 flex-col items-center gap-[60px]'>
-        <Header />
-        {children}
-        <Footer />
+    <div className="flex m-auto w-[1440px] pb-0 flex-col items-center gap-[60px]">
+      <Header />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
