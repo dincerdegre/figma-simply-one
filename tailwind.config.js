28ea529e9485f36a8a385e7ad3c1fe84527793fd
dincerdegre <dincerdegre@gmail.com>
2023-07-04 05:58:29 +0300

    added bg images

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,9 @@ module.exports = {
         generalsubstrate: '#F9FAFC',
       },
       backgroundImage: {
-        'second-hero': 'url("assets/images/64542dfdb7b8896bfd708e7a22e49cd3.jpg")',
+        'video-image': 'url("assets/images/f576b710d54d9756878c0c770e0be394.png")',
+        'list-image': 'url("assets/images/1aedffed1513982be7ca74c5286a8b97.jpg")',
+        'wide-image': 'url("assets/images/64542dfdb7b8896bfd708e7a22e49cd3.jpg")',
       },
     },
   },
