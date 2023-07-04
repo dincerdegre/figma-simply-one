c97e1f4c25b148964527e4e61edad33c10287d7a
dincerdegre <dincerdegre@gmail.com>
2023-07-05 00:03:59 +0300

    added info page

--- src/pages/Info/Info.tsx
+++ src/pages/Info/Info.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import WideLogo from "../../components/InfoPage/WideLogo/WideLogo";
+import Heading from "../../components/InfoPage/Heading/Heading";
+import Substrate from "../../components/Shared/Substrate/Substrate";
+import TypoColor from "../../components/InfoPage/TypoColor/TypoColor";
+import Frame from "../../components/InfoPage/Frame/Frame";
+import Blockquote from "../../components/Shared/Blockquote/Blockquote";
 
 type Props = {};
 
 const Info = (props: Props) => {
-  return <div>Info</div>;
+  return (
+    <>
+      <WideLogo />
+      <Heading text="Tokens" />
+      <Substrate />
+      <TypoColor />
+      <Substrate />
+      <Heading text="General Symbols" />
+      <Frame />
+      <Blockquote text="Welcome! Here imagination meets innovation, and your aspirations find their ultimate expression!" />
+    </>
+  );
 };
 
 export default Info;
