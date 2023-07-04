cd1879a04023771843866d40aad4be3b38cacecf
dincerdegre <dincerdegre@gmail.com>
2023-07-05 00:03:35 +0300

    added widelogo component

---
+++ src/components/InfoPage/WideLogo/WideLogo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WideLogo: React.FC = () => {
+  return <div className="w-[1440px] h-[480px] bg-wide-logo bg-cover bg-center"></div>;
+};
+
+export default WideLogo;
